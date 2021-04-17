a869baeff1f7ee66577c2630dc2748b7e4aeb3a1
Mukundan-K <78027142+Mukundan-K@users.noreply.github.com>
2021-04-17 10:46:50 +0530

    Update Authordata.js

--- Backend/src/model/Authordata.js
+++ Backend/src/model/Authordata.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://userone:userone@cluster0.tixpa.mongodb.net/LIBRARYAPP?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://userone:userone@ictakfiles.kialy.mongodb.net/LIBRARYAPP?retryWrites=true&w=majority');
 const schema = mongoose.Schema;
 
 const AuthorSchema = new schema({
